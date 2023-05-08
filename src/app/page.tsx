f32176c7fb017e773b3e5f28ac51cf54afda8a43
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-05-08 00:10:29 -0300

    Cards added!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,38 @@
-import Image from "next/image";
-
+import ItemCard from "./components/itemCard";
+import SquareCard from "./components/squareCard";
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col gap-8">
-        <div className="w-full h-96 bg-gray-900 flex justify-center items-center text-white">
-          <h1>Iphone 15S</h1>
-        </div>
+        <div
+          className="w-full h-96 bg-gray-900 flex justify-center items-center text-white shadow-lg"
+          style={{
+            backgroundImage: "url('/banners/iphone.jpg')",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            width: "100%",
+          }}
+        ></div>
 
         <div className="flex justify-center">
           <div className="grid grid-cols-2  gap-4 items-center w-4/5">
-            <div className="bg-black max-w-4xl aspect-square">Box 1</div>
-            <div className="bg-black max-w-4xl aspect-square">Box 1</div>
+            <SquareCard imageURL={"/placeholder.jpg"} href="123" />
+            <SquareCard imageURL={"/placeholder.jpg"} href="123" />
           </div>
         </div>
 
         <div className="flex justify-center mb-10">
           <div className="grid grid-cols-4 gap-6">
-            <div className="h-96 w-80 bg-black">Block</div>
-            <div className="h-96 w-80 bg-black">Block</div>
-            <div className="h-96 w-80 bg-black">Block</div>
-            <div className="h-96 w-80 bg-black">Block</div>
+            <ItemCard itemID="123" title="Airpods" />
+            <ItemCard itemID="123" title="Airpods" />
+            <ItemCard itemID="123" title="Airpods" />
+            <ItemCard itemID="123" title="Airpods" />
 
-            <div className="h-96 w-80 bg-black">Block</div>
-            <div className="h-96 w-80 bg-black">Block</div>
-            <div className="h-96 w-80 bg-black">Block</div>
-            <div className="h-96 w-80 bg-black">Block</div>
+            <ItemCard itemID="123" title="Airpods" />
+            <ItemCard itemID="123" title="Airpods" />
+            <ItemCard itemID="123" title="Airpods" />
+            <ItemCard itemID="123" title="Airpods" />
           </div>
         </div>
       </div>
