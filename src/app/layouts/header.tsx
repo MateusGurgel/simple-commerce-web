dbcbe7dbef63f1337a96da3f6dd11f31943ef4ef
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-05-13 13:56:02 -0300

    Sign up button added on header

--- src/app/layouts/header.tsx
+++ src/app/layouts/header.tsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <header className="flex items-center justify-center w-full h-12 bg-black text-white">
       <div className="flex justify-between w-full max-w-screen-2xl px-5">
-        <h1 className="font-bold text-xl">SC</h1>
+        <Link href="/">
+          <h1 className="font-bold text-xl">SC</h1>
+        </Link>
 
         <nav className="flex gap-10">
           <Link className="font-light font-sans text-base" href="cart">
@@ -13,6 +15,9 @@ export default function Header() {
 
           <Link className="font-light font-sans text-base" href="login">
             Sing in
+          </Link>
+          <Link className="font-light font-sans text-base" href="register">
+            Sign up
           </Link>
         </nav>
       </div>
