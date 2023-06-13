2a4651edeac3c73e2d7524f9535305c2e73b7bd2
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-06-13 14:47:48 -0300

    PayPal provider is set up

--- src/app/providers.js
+++ src/app/providers.js
@@ -5,15 +5,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 export function Providers({ children }) {
   return (
     <CacheProvider>
       <ChakraProvider>
         <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            {children}
-          </PersistGate>
+          <PayPalScriptProvider
+            options={{ clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID }}
+          >
+            <PersistGate loading={null} persistor={persistor}>
+              {children}
+            </PersistGate>
+          </PayPalScriptProvider>
         </Provider>
       </ChakraProvider>
     </CacheProvider>
