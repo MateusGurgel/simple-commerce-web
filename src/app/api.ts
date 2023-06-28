900aed3df18bee127fc420b6e50e00bd6883fa3f
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-06-27 21:09:09 -0300

    endPoint const created

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosResponse, AxiosError } from "axios";
+import { endPoint } from "./constants";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333",
+  baseURL: endPoint,
 });
 
 function setApiToken(token: string) {
